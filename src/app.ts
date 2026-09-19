73446078f8f3be2a12f772eb7a7c184d110154fa
Bruno <br.delani@gmail.com>
2026-09-19 14:29:32 -0400

    feat: connect cloud mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import './database/connection.js'
 import 'dotenv/config';
 
 
---
+++ src/database/connection.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+class Database {
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        const dbConnectionPath: string = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER}.${process.env.DATABASE_CODE}.mongodb.net/`;
+        mongoose.connect(dbConnectionPath);
+        mongoose.connection.on('error', () => {
+            console.log('Could not connect to database.');
+        });
+        mongoose.connection.once('open', () => {
+            console.log(`Connection established with the database ${process.env.DATABASE_CLUSTER}.`);
+        });
+        return mongoose.connection;
+    }
+}
+
+export default new Database().connect;
